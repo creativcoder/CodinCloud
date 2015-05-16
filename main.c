4cb7d020a383d605d08eb037842c811da90ec54d
Rahul <rsconceptx@gmail.com>
2015-05-16 22:19:34 +0530

    Reworked the logic

--- main.c
+++ main.c
@@ -1,17 +1,5 @@
 #include<stdio.h>
-void printpattern(int n)
-{
-int i,j,k;
-for(i=0;i<n;i++)
-{ for(k=n;k>i;k--)
-{printf(" ");}
-for(j=0;j<i;j++)
-{printf("* ");}
-printf("\n");
-}
-}
 int main()
 {
-int n=20;
-printpattern(n);
+printf("Hell owrld");
 }
