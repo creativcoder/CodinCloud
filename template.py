f98a34a2ba2a287872fdf4fbb72fe5a76cbd64f7
Rahul Sharma <rsconceptx@gmail.com>
2015-09-29 12:07:45 +0530

    resolved stdin errors

---
+++ template.py
@@ -0,0 +1,6 @@
+code_templates = {
+	'c':'#include<stdio.h> int main(){printf(""); return 0;}',
+	'java':'public class Main{public static void main(String[] args){System.out.println("");}}',
+	'python':'print("")',
+	'rust':'fn main(){println!({},"");}',
+}
--- tests/ps.py
+++ tests/ps.py
@@ -1,15 +1,26 @@
 import subprocess
+from subprocess import PIPE,Popen
 import psutil
 import os
 import time
-TIMEOUT = 1
-subp = subprocess.Popen([os.path.abspath('a.out')])
+TIMEOUT = 2
 
+inputs = ['23','45','34','46']
 
+subprocess.Popen(['gcc',os.path.abspath('main.c')])
+time.sleep(0.4)
+subp = subprocess.Popen([os.path.abspath('a.out')],stdin=PIPE,stdout=PIPE,stderr=PIPE)
+
+for i in inputs:
+	subp.stdin.write(i)
+subp.stdin.write('\n')
+print(subp.stdout.read())
+print("communicate ran")
 p = psutil.Process(subp.pid)
 while 1:
     if (time.time()-p.create_time())>TIMEOUT:
         p.kill()
+        print("This was an infinte loop")
+        raise RuntimeError('timeout')
         break
-        #raise RuntimeError('timeout')
-    time.sleep(5)
+        
