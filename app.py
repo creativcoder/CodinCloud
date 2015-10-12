eea035806753f098e20df3a75bef4b89884d54be
Rahul Sharma <rsconceptx@gmail.com>
2015-10-12 23:50:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,21 +9,6 @@ import subprocess
 
 # from jinja2 import evalcontextfilter, Markup, escape
 
-# _paragraph_re = re.compile(r'(?:\r\n|\r|\n){2,}')
-
-# app = Flask(__name__)
-
-# @app.template_filter()
-# @evalcontextfilter
-# def nl2br(eval_ctx, value):
-#     result = u'\n\n'.join(u'<p>%s</p>' % p.replace('\n', '<br>\n') \
-#         for p in _paragraph_re.split(escape(value)))
-#     if eval_ctx.autoescape:
-#         result = Markup(result)
-#     return result
-
-
-
 from flask import Flask,request,render_template,url_for,jsonify
 
 logging.basicConfig(level=logging.DEBUG)
@@ -71,7 +56,6 @@ def test():
 	logging.debug(output_result)
 	time.sleep(0.5)
 	return jsonify(result=output_result.replace('\n','<br>'))
-	#logging.debug(output.stdout.read())
 
 @app.route('/about')
 def about():
