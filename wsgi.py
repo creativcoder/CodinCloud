b92ffa5492de121318298aa3088112a3f7cd2ca6
Rahul Sharma <rsconceptx@gmail.com>
2016-02-06 21:40:27 +0530

    some minor nits

--- wsgi.py
+++ wsgi.py
@@ -5,6 +5,9 @@ from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 
+PORT = 5000
+
 http_server = HTTPServer(WSGIContainer(app))
-http_server.listen(5000)
+http_server.listen(PORT)
+print("Serving CodinCloud on {}".format(PORT))
 IOLoop.instance().start()
