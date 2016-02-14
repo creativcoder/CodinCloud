7d1b92be4c324cca5e0e32c72682477b5085e7b6
Rahul Sharma <rsconceptx@gmail.com>
2016-02-14 21:42:12 +0530

    adding a automation script and updating readme

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,5 @@
+import sys
+
 from CodinCloud import app as ide_instance
 app = ide_instance.app
 
@@ -5,7 +7,7 @@ from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 
-PORT = 5000
+PORT = 5000 if len(sys.argv)<2 else sys.argv[1]
 
 http_server = HTTPServer(WSGIContainer(app))
 http_server.listen(PORT)
